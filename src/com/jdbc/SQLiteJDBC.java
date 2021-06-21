1fe5803d4694b882afb94d55f67340179eb625d7
timofeya123 <timofeya123@inbox.ru>
2021-06-21 20:28:45 +0600

    comment

--- src/com/jdbc/SQLiteJDBC.java
+++ src/com/jdbc/SQLiteJDBC.java
@@ -5,28 +5,28 @@ import java.sql.*;
 public class SQLiteJDBC {
 
     public static void main( String args[] ) {
-        Connection c = null;
-        Statement stmt = null;
+        Connection c = null; // соединение
+        Statement stmt = null; // поток
 
         try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:test.db");
-            System.out.println("Opened database successfully");
+            Class.forName("org.sqlite.JDBC"); // формат бд
+            c = DriverManager.getConnection("jdbc:sqlite:test.db"); // выбираем бд из фалов и соединяемся
+            System.out.println("Открыта БД, успех!");
 
-            stmt = c.createStatement();
+            stmt = c.createStatement(); //бд в поток
             String sql = "CREATE TABLE COMPANY " +
                     "(ID INT PRIMARY KEY     NOT NULL," +
                     " NAME           TEXT    NOT NULL, " +
                     " AGE            INT     NOT NULL, " +
                     " ADDRESS        CHAR(50), " +
-                    " SALARY         REAL)";
-            stmt.executeUpdate(sql);
-            stmt.close();
+                    " SALARY         REAL)"; // создание таблицы в sql
+            stmt.executeUpdate(sql); // обновить бд
+            stmt.close(); // закрыть
             c.close();
         } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() ); // ошибка
             System.exit(0);
         }
-        System.out.println("Table created successfully");
+        System.out.println("Таблица создана, успех!");
     }
 }
